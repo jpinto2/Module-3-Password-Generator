4d3f56dbe6e6d4cb8e1614ad762b75c41c9894a3
jpinto3 <jpinto3@mail.csuchico.edu>
2022-10-02 14:52:53 -0700

    setup prompts and some console.log statements testing it

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 // Assignment code here
+function generatePassword() {
 
+  var select = [];
+  
+  var lower = window.prompt(" Do you want to include lowercase letters? If so, enter y");
+
+  if (lower === "y") select.push("abcdefghijklmnopqrstuvwxyz");
+
+  var upper = window.prompt("Do you want to include uppercase letters? If so, enter y");
+
+  if (upper === "y") select.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+  var numbers = window.prompt("Do you want to include numbers? If so, enter y");
+
+  if (numbers === "y") select.push("1234567890");
+
+  var special = window.prompt("Do you want to include special characters? If so, enter y");
+
+  if (special === "y") var count = select.push("`~!@#$%^&*()-_=+[{]}|;:',<.>/?");
+
+  
+  console.log("bacon");
+  console.log(count);
+  console.log(select[0]);
+  console.log(select[0][0]);
+
+  return select[0];
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
