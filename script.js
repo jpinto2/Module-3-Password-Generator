eab9302289e01d5a4292fb4c696a5e0cf4368c4a
jpinto3 <jpinto3@mail.csuchico.edu>
2022-10-03 20:09:51 -0700

    comments added to javascript file. final commit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Assignment code here
 function generatePassword() {
 
+  // select is my array of strings holding all the character types to choose from. password is the random password sting generated. both are intialized to [] to make them empty arrays
   var select = [];
   var password = [];
+
+  // i is my variable for for loops later
   var i;
+
+  // the random i and random j needed for selecting the indexes of a random character
+  // randI will decide type of criteria(type of charcter). randJ will decide which character in that criteria
   var randI;
   var randJ;
   
+  // a do while loop to make sure user picks at least one criteria for password
   do {
+
+    //asks the user for each criteria. if they answer yes, we add the characters in that criteria to the array
     var lower = window.prompt(" Do you want to include lowercase letters? If so, enter y");
 
     if (lower === "y") select.push("abcdefghijklmnopqrstuvwxyz");
@@ -26,23 +35,28 @@ function generatePassword() {
 
   } while(select.length === 0);
 
+  // do-while loop makes sure valid number is picked for size of random password
   do {
 
     var size = Number(window.prompt("Enter the size of your password. You may choose from 8 to 128"));
 
   } while(typeof size != "number" || size < 8 || size > 128);
 
+  // each criteria must be used at least once so first for loop insures that by selecting one random character from each criteria
   for(i=0; i<select.length; i++) {
     randJ = Math.floor(Math.random() * select[i].length);
+    // password is single string so we can just concatenate the new character to current password
     password += select[i][randJ];
   }
 
+  // this for loop fills up the remaining space in password with random character from randomly chosen criteria
   for(i=select.length; i<size; i++) {
     randI = Math.floor(Math.random() * select.length);
     randJ = Math.floor(Math.random() * select[randI].length);
     password += select[randI][randJ];
   }
 
+  // return password now that it's complete
   return password;
 
 }
